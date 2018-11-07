45d596494615743eca7f291b48ffeb541fc1da69
raven <raven.nguyen@beyondedge.co>
2018-11-07 09:15:21 +0700

    setting

--- nodejs-v2/setting.js
+++ nodejs-v2/setting.js
@@ -16,7 +16,7 @@ module.exports = {
     tempFolder: 'tmp', // folder to store scratch files
     server: {
         http: {
-            port: 81,
+            port: 8080,
         },
         https: { // https://nodejs.org/api/https.html#https_https_createserver_options_requestlistener
             port: 443,
@@ -73,8 +73,7 @@ module.exports = {
     },
     database: {
         mongo: {
-            //url: 'mongodb://admin:vrp123456@127.0.0.1:27017/cvrp',
-            url: 'mongodb://192.168.50.150:27017/cvrp',
+            url: 'mongodb://192.168.50.211:27017/cvrp',
         },
         // sql: { // http://docs.sequelizejs.com/class/lib/sequelize.js~Sequelize.html#instance-constructor-constructor
         //     username: 'sa',
@@ -89,9 +88,9 @@ module.exports = {
         //     },
         // },
         sql: {
-            username: 'mcloyalty',
-            password: '@Teg123@',
-            host: '115.78.6.171',
+            username: 'gls',
+            password: 'G!Ukd^7f#SpAJ&eo',
+            host: 'VM-BESV2',
             port: 1433,
             dialect: 'mssql',
             database: 'GLS_Test',
